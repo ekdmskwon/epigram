a3e61f89ae45488343c08c799651b0acc713a451
ekdmskwon <dadax0715@gamil.com>
2026-05-26 23:59:45 +0900

    feat: axios 자동 설정을 위해 이미지 업로드 헤더 제거

--- api/image.ts
+++ api/image.ts
@@ -5,22 +5,17 @@ export interface UploadImageResponse {
 }
 
 /**
- * @params file 유저가 선택한 이미지 파일 객체
+ * @param file 유저가 선택한 이미지 파일 객체
  */
 export const uploadImage = async (file: File): Promise<UploadImageResponse> => {
-  const formData = new FormData;
+  const formData = new FormData();
 
   formData.append("image", file);
 
-  const response = await instance.post<UploadImageResponse>(  
+  const response = await instance.post<UploadImageResponse>(
     "/images/upload",
     formData,
-    {
-      headers: {
-        "Content-Type": "multipart/form-data",
-      },
-    }
   );
 
   return response.data;
-}
+};
