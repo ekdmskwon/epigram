e6a8710ebabe1335c7e804e6ef4d8f8138a19d59
ekdmskwon <dadax0715@gamil.com>
2026-05-29 01:26:43 +0900

    chore: 원격 저장소에서 관련 없는 파일 제외

--- src/app/login/type.d.ts
+++
@@ -1,7 +0,0 @@
-import { ButtonHTMLAttributes, ReactNode } from "react";
-
-export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  variant?: "main" | "wide";
-  size?: "sm" | "md" | "lg";
-  children: ReactNode;
-}
